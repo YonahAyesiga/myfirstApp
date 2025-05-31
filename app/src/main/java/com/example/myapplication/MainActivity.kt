ea5626fa4278863f73d909346a0f06dcb6cb4cd6
YonahAyesiga <yonahayesiga241@gmail.com>
2025-05-31 10:24:01 +0300

    my second commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+     val etUsername = findViewById<EditText>(R.id.etUsername)
+     val etpassword = findViewById<EditText>(R.id.etPassword)
+        val login = findViewById<Button>(R.id.btnlogin)
+
+        login.setOnClickListener{
+            val username = etUsername.text.toString().trim()
+            val password = etpassword.text.toString().trim()
+            if(username.isEmpty()){
+                Toast.makeText(this, "Please enter the username", Toast.LENGTH_SHORT).show()
+            }
+             else if(password.isEmpty()){
+                Toast.makeText(this,"Please enter the password", Toast.LENGTH_SHORT).show()
+            } else if(username == "admin" && password =="123"){
+                Toast.makeText(this, "Login Successful",Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                finish()
+            } else{
+                Toast.makeText(this, "Incorrect username or password", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
